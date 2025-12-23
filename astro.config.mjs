77ad5aa7b3e72faaf5fc75aaed1b49bb3fcb3a9e
Fraser Hess <fraser@renewedheart.org>
2025-12-22 19:09:57 -0700

    GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://setup-manager-fansite.github.io',
 	integrations: [
 		starlight({
 			title: 'Setup Manager Fansite',
